3cb4ed72593ceb33668b85a2ba2b8c96b34e00e9
harsh-185 <harsh.maheshwari180504@gmail.com>
2024-07-08 13:37:40 -0400

    commit

--- src/screens/User/ContactInfoScreen.js
+++ src/screens/User/ContactInfoScreen.js
@@ -13,13 +13,13 @@ import {
 
 const ContactInfoScreen = () => {
   const handlePhonePress = (number) => {
-    if (Platform.OS === "android") {
-      Linking.openURL(`tel:${number}`);
-    } else if (Platform.OS === "ios") {
-      Linking.openURL(`telprompt:${number}`);
-    } else {
-      console.log("Phone number is not supported");
-    }
+    // if (Platform.OS === 'android') {
+    Linking.openURL(`tel:${number}`);
+    // } else if (Platform.OS === 'ios') {
+    //   Linking.openURL(`telprompt:${number}`);
+    // } else {
+    //   console.log('Phone number is not supported');
+    // }
   };
 
   const handleAddressPress = (address) => {
@@ -31,7 +31,7 @@ const ContactInfoScreen = () => {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Image source={require("../../images/logo.png")} style={styles.logo} />
+      <Image source={require("../images/logo.png")} style={styles.logo} />
       <Text style={styles.title}>Congenital Heart Center Clinic List</Text>
       <Text style={styles.sectionTitle}>
         Pediatric Cardiology/Congenital Heart Group
