2f6959320c3ea769d48cb0fc26812b952ea2447e
Bradie Fernandez <bradiefuentes@Bradies-MacBook-Pro.local>
2024-07-11 13:48:29 -0400

    cache comments

--- src/cache/CacheHandler.js
+++ src/cache/CacheHandler.js
@@ -4,18 +4,26 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const METADATA_KEY_PREFIX = "video_metadata_";
 
+// Function to delete cached video
 export const deleteCachedVideo = async (videoId) => {
   try {
+    // Retrieve metadata for the video from AsyncStorage
     const metadata = await AsyncStorage.getItem(METADATA_KEY_PREFIX + videoId);
+    // If metadata exists for the video
     if (metadata) {
+      // analyze metadata to get the video path
       const { path } = JSON.parse(metadata);
       if (await RNFS.exists(path)) {
+        // If the video file exists, unlink/delete it
         await RNFS.unlink(path);
         console.log(`Deleted cached video at path: ${path}`);
       }
+      // Remove the metadata entry from AsyncStorage
       await AsyncStorage.removeItem(METADATA_KEY_PREFIX + videoId);
+      // Log a message confirming metadata deleted
       console.log(`Deleted metadata for video ID: ${videoId}`);
     }
+    // Catch and log any errors during the deletion process
   } catch (error) {
     console.error("Failed to delete cached video:", error);
   }
--- src/cache/MetadataHandler.js
+++ src/cache/MetadataHandler.js
@@ -4,24 +4,32 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const METADATA_KEY_PREFIX = "video_metadata_";
 
+// get video metadata based on videoId
 export const getVideoMetadata = async (videoId) => {
   try {
+     // get metadata from AsyncStorage using the given key
     const metadata = await AsyncStorage.getItem(METADATA_KEY_PREFIX + videoId);
+    // analyze and return metadata if exists, otherwise return null
     return metadata ? JSON.parse(metadata) : null;
   } catch (error) {
+    // handle errors during AsyncStorage access
     console.error("Failed to get video metadata:", error);
     return null;
   }
 };
 
+// save video metadata to AsyncStorage
 export const saveVideoMetadata = async (videoId, path) => {
   try {
+    // create metadata object with a path and current timestamp
     const metadata = { path, timestamp: new Date().toISOString() };
+   // save the metadata to AsyncStorage under the given key
     await AsyncStorage.setItem(
       METADATA_KEY_PREFIX + videoId,
       JSON.stringify(metadata)
     );
   } catch (error) {
+    // Handle errors during AsyncStorage access
     console.error("Failed to save video metadata:", error);
   }
 };
